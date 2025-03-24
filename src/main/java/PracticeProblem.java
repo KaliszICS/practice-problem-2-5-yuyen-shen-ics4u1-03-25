aec5c68b873513a6a907f942308881455b093047
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-03-24 19:03:31 +0000

    yuyen

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,21 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		insertionSort(new char[] {'b', 'e', 'a'});
 	}
 
+	public static void insertionSort(char[] characters) {
+		for (int a = 1; a < characters.length; a++) {
+			char key = characters[a];
+			int index = a - 1;
+			while (index >= 0 && characters[index] > key) {
+				characters[index + 1] = characters[index];
+				index--;
+			}
+			characters[index + 1] = key;
+
+		}
+
+	}
 
 }
